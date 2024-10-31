0d2c5e9a74aec55d3a79ff3704e06d0fb9b2028f
Ming Gong <ming@neuron.sg>
2024-10-31 17:44:14 +0800

    [MG]: Code clean

--- src/test/java/com/whatsapp/api/TestConstants.java
+++ src/test/java/com/whatsapp/api/TestConstants.java
@@ -3,13 +3,13 @@ package com.whatsapp.api;
 /*fill the variables with your data*/
 public class TestConstants {
 
-    public final static String TOKEN = "EAAL7s3z1tYEBOyK3FfZB07n8qZA0uzfOAjGgYxQxjtEDd2z57KrBzmD5mDZALkVnyJdTFr5yr6QBsagQhDppaMFDntdzAJKTmREtXPPcDbV3RolghgGSvuDbGA5IbVhxP7tBVvMxNVPZCZA7YCcB7NcgioQjBxZB2QSLl7QJNwLTiNWildwlBPEpyuk37rzbSZBVAZDZD";
+    public final static String TOKEN = "xxxxxxxxxxxxxxxx";
 
     public final static String WABA_ID = "your_waba_id";
-    public final static String PHONE_NUMBER_1 = "6581975095";
+    public final static String PHONE_NUMBER_1 = "xxxxxxxxxxxxxxx";
     public final static String PHONE_NUMBER_2 = "a_phone_number_to_test";
     public final static String PHONE_NUMBER_3 = "a_phone_number_to_test";
     public final static String PHONE_NUMBER_4 = "a_phone_number_to_test";
     public final static String PHONE_NUMBER_5 = "a_phone_number_to_test";
-    public final static String PHONE_NUMBER_ID = "470480436139778";
+    public final static String PHONE_NUMBER_ID = "xxxxxxxxxxxxxxxx";
 }
